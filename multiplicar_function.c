31181e521f236ca7aa86c0ed3d59081ec2d118dd
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2025-10-27 10:16:25 -0300

    Implement multiplication function using loops

---
+++ multiplicar_function.c
@@ -0,0 +1,16 @@
+int multiplicar(int numero1,int numero2)
+{
+    contador1 = 0; 
+    contador2 = 0; 
+    produto = 0; 
+
+    while (contador1 < numero1){ 
+        contador2 = 0; 
+        while (contador2 < numero2){ 
+            produto++; // produto = produto + 1; 
+            contador2++;    
+        } 
+        contador1++;     
+    }
+    return produto;
+}
