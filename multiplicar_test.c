81724a074b54234f911ad276c3fe35a46cc3867a
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2025-10-27 10:24:35 -0300

    Add test for multiplicar function

---
+++ multiplicar_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+#include "multiplicar_function.c"
+
+int main()
+{
+  int produto = multiplicar(5,3);
+  assert(produto == 15);
+  printf("1 Teste 1 Assertiva OK").
+  return 0;  
+}
