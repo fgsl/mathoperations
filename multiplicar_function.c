bb539080477b331876c1f663f01b8af30426e851
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2025-10-27 10:28:42 -0300

    Declare variables with proper types in multiplicar

--- multiplicar_function.c
+++ multiplicar_function.c
@@ -1,8 +1,8 @@
 int multiplicar(int numero1,int numero2)
 {
-    contador1 = 0; 
-    contador2 = 0; 
-    produto = 0; 
+    int contador1 = 0; 
+    int contador2 = 0; 
+    int produto = 0; 
 
     while (contador1 < numero1){ 
         contador2 = 0; 
